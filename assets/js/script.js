f6fb59964e71b68750e8d380e0d06d10430b8805
Callee84 <carl.g.holm@gmail.com>
2022-09-21 21:17:53 +0000

    add form and answer box to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(){
                 console.log("Estate")
             }
             else if (this.getAttribute("data-type") === "appartment") {
+                alert(`Right... You accually need to own the building in order to put up solar panels. Please contact your landlord.`)
+                throw `Sorry, we don't do appartments`;
                 console.log("Appartment")
             }
         }    
