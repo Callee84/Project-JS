856027ad4a37e938197bba3ad11ad308f5aae3c0
Callee84 <carl.g.holm@gmail.com>
2022-09-22 06:52:50 +0000

    add div for total sum

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
 
@@ -5,17 +7,24 @@ document.addEventListener("DOMContentLoaded", function(){
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "house") {
                 console.log("House")
-            } else if (this.getAttribute("data-type") === "estate") {
-                console.log("Estate")
+            } else if (this.getAttribute("data-type") === "industry") {
+                console.log("Industry")
             }
             else if (this.getAttribute("data-type") === "appartment") {
                 alert(`Right... You accually need to own the building in order to put up solar panels. Please contact your landlord.`)
                 throw `Sorry, we don't do appartments`;
                 console.log("Appartment")
             }
-        }    
-        )
+        })
     }
+})
 
+/**
+ * calulation for prices
+ * there is different prices depending on property
+ */
+
+function calculatePrice() {
+    let userAnswer = document.getElementById("size-house"),value;
+   
 }
-)
