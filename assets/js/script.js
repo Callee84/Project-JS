b12bebda1f324e57fa2791e32b0a6b41de5386ba
Callee84 <carl.g.holm@gmail.com>
2022-09-23 19:55:12 +0000

    add structure, style & javascript for slideshow

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,23 +27,14 @@ document.addEventListener("DOMContentLoaded", function(){
  * calculating price for solar panels
  */
 
-document.getElementById("calculation").addEventListener("submit", computeResults);
 
-function computeResults(e) {
-    
-    const UIamount = document.getElementById("size-prop").value;
-
-    const principal = parseFloat(UIamount);
-    
-
-}
 /**
  * Pop- up window for contact information
  */
 
 // Create reference to button and popup element
 
-const button = document.querySelector("button");
+const button = document.querySelector("#pop-button");
 const popup = document.querySelector(".popup-wrapper");
 const close = document.querySelector(".popup-close");
 
@@ -61,7 +52,31 @@ close.addEventListener("click", () => {
 });
 
 // Close popup when clicking outside the element
-popup.addEventListener("click", () => {
-  popup.style.display = "none";
-});
+// popup.addEventListener("click", () => {
+//   popup.style.display = "none";
+// });
 
+/**
+ * Slidesshow aninmation
+ * change img every 4 seconds
+ */
+
+let slideImg = 0;
+showImg();
+
+function showImg() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot"); 
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideImg++;
+    if (slideImg > slides.length) {slideImg = 6}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active","");
+    }
+    slides[slideImg-1].style.display = "block";
+    dots[slideImg-1].className += "active";
+    setTimeout(showImg, 4000);
+}
